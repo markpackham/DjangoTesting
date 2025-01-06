9554c4c0acd62547dbfbc8a93ce1d37cb7e1502c
M <markpackham1@gmail.com>
2025-01-06 09:31:28 +0000

    Add get_discounted_price()
    https://youtu.be/duYxizXokZM?feature=shared&t=326

--- testing_project/products/tests.py
+++ testing_project/products/tests.py
@@ -11,6 +11,16 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_count = models.IntegerField(default=0)
 
+    def get_discounted_price(self, discount_percentage):
+        """ Calculate & return the discounted price """
+        return self.price * (1 - discount_percentage / 100)
+
+    @property
+    def in_stock(self) -> bool:
+        return self.stock_count > 0
+
     def clean(self):
         if self.price < 0:
-            raise
+            raise ValidationError('Price cannot be negative')
+        if self.stock_count < 0:
+            raise ValidationError('Stock Count cannot be negative')
