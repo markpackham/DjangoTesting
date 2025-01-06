be386caab3a1e376622e1c59380e7c3c628525d1
M <markpackham1@gmail.com>
2025-01-06 10:00:49 +0000

    Add TestProduct test
    https://youtu.be/duYxizXokZM?feature=shared&t=496

--- testing_project/products/test/test_models.py
+++ testing_project/products/test/test_models.py
@@ -2,3 +2,6 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 from products.models import Product
 
+class ProductModelTest(TestCase):
+    def setUp(self):
+        self.product = Product(name='Test Product', price=100, stock_count=10)
