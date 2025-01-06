7488a9b4ad3b4134f9bf6498f321b859225748ef
M <markpackham1@gmail.com>
2025-01-06 10:04:09 +0000

    Test stock_property
    https://youtu.be/duYxizXokZM?feature=shared&t=564

--- testing_project/products/test/test_models.py
+++ testing_project/products/test/test_models.py
@@ -5,3 +5,9 @@ from products.models import Product
 class ProductModelTest(TestCase):
     def setUp(self):
         self.product = Product(name='Test Product', price=100, stock_count=10)
+
+    def test_in_stock_property(self):
+        self.assertTrue(self.product.in_stock)
+        # Set stock_count to 0 & test again
+        self.product.stock_count = 0
+        self.assertFalse(self.product.in_stock)
