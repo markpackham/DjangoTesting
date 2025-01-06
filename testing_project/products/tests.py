071d1adf2817e2ba56494ba41acc688c027f75d0
M <markpackham1@gmail.com>
2025-01-06 09:18:55 +0000

    Import ValidationError
    https://youtu.be/duYxizXokZM?feature=shared&t=194

--- testing_project/products/tests.py
+++ testing_project/products/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
 class User(AbstractUser):
@@ -9,3 +10,7 @@ class Product(models.Model):
     name = models.CharField(max_length=128)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_count = models.IntegerField(default=0)
+
+    def clean(self):
+        if self.price < 0:
+            raise
