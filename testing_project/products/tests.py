e82fab8145d319e78c6b90b0d3b8b6d0b108a063
M <markpackham1@gmail.com>
2025-01-06 08:49:18 +0000

    Add Product test
    https://youtu.be/duYxizXokZM?feature=shared&t=170

--- testing_project/products/tests.py
+++ testing_project/products/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import AbstractUser
 
 # Create your tests here.
+class User(AbstractUser):
+    pass
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_count = models.IntegerField(default=0)
