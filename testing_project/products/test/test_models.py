f7015a8b00191a9fbc8fd7e5d569d5ee4f1abe0d
M <markpackham1@gmail.com>
2025-01-06 10:22:46 +0000

    Test negative_price_validation
    https://youtu.be/duYxizXokZM?feature=shared&t=787

--- testing_project/products/test/test_models.py
+++ testing_project/products/test/test_models.py
@@ -16,3 +16,10 @@ class ProductModelTest(TestCase):
         self.assertEqual(self.product.get_discounted_price(10), 90)
         self.assertEqual(self.product.get_discounted_price(50), 50)
         self.assertEqual(self.product.get_discounted_price(0), 100)
+
+    def test_negative_price_validation(self):
+        self.product.price = -10
+        # Make sure we get a validation error
+        with self.assertRaises(ValidationError):
+            # When we expect to raise the validation error
+            self.product.clean()
