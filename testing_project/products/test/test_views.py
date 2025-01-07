36a7f9fe4a75f12f01f9d9bba91f60fceea90bb3
M <markpackham1@gmail.com>
2025-01-07 10:43:52 +0000

    Test homepage_contains_welcome_message()
    https://youtu.be/la69esudPYY?si=Taci0nVjmQRCeFCz&t=552

--- testing_project/products/test/test_views.py
+++ testing_project/products/test/test_views.py
@@ -5,3 +5,12 @@ class TestHomePage(SimpleTestCase):
     def test_homepage_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_homepage_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
+
+# THIS IS AMAZING - make sure a web page contains a specific message!!!!
+    def test_homepage_contains_welcome_message(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Welcome to our Store!')
