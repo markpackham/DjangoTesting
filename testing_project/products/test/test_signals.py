187712abf00076fbe7d3af9bc56f201ea3e063bd
M <markpackham1@gmail.com>
2025-01-09 12:19:39 +0000

    Add welcome_email_sent_on_user_creation()
    https://youtu.be/DWMM0IJ2uNw?si=fZXmRVL_6YI359wZ&t=372

---
+++ testing_project/products/test/test_signals.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from unittest.mock import patch
+from products.models import User
+
+class UserSignalsTest(TestCase):
+
+    # Mock functionality
+    @patch('products.signals.send_mail')
+    def test_welcome_email_sent_on_user_creation(self, mock_send_mail):
+        # Create new user to trigger signal
+        User.objects.create_user(username='john', email='john@email.com',password='password123')
+
+        # Check send_mail called once
+        mock_send_mail.assert_called_once_with(
+
+        )
