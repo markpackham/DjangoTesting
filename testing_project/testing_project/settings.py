c23c5833a95839f63a0649468b80a05af2910184
M <markpackham1@gmail.com>
2025-01-09 13:05:51 +0000

    Put site in maintenance mode
    https://youtu.be/3tHHTnLqZfA?si=X9X7RhzwHBIg6Z5N&t=261

--- testing_project/testing_project/settings.py
+++ testing_project/testing_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'products.middleware.MaintenanceModeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'products.User'
 LOGIN_URL = '/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MAINTENANCE_MODE = True
