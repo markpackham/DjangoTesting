1529fa295db21ba876893853cb2591013c66541e
M <markpackham1@gmail.com>
2025-01-09 13:24:50 +0000

    Test maintenance_mode_on()
    https://youtu.be/3tHHTnLqZfA?si=yue8r7bEMN2C-trJ&t=546

--- testing_project/products/test/test_middleware.py
+++ testing_project/products/test/test_middleware.py
@@ -1,11 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 class MaintenanceModeTests(TestCase):
 
+    @override_settings(MAINTENANCE_MODE=False)
     def test_maintenance_mode_off(self):
         # Test site works normally when maintenance mode set to false
         response = self.client.get(reverse('homepage'))
 
         # Check response successful & normal content returned
         self.assertContains(response, 'Welcome to our Store!', status_code=200)
+
+
+    @override_settings(MAINTENANCE_MODE=True)
+    def test_maintenance_mode_on(self):
+        response = self.client.get(reverse('homepage'))
+
+        self.assertContains(response, 'Site is under maintenance', status_code=503)
